35cb818f704b5125dce910bb2da5eccd24585c58
mayaralm <mmayaraleitedemelo@gmail.com>
2022-02-18 17:07:54 -0300

    criadas rotas post, put e delete

--- sistema-financeiro/src/main/java/com/example/sistemafinanceiro/resourses/ClientesResourses.java
+++ sistema-financeiro/src/main/java/com/example/sistemafinanceiro/resourses/ClientesResourses.java
@@ -3,10 +3,15 @@ package com.example.sistemafinanceiro.resourses;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +36,16 @@ public class ClientesResourses {
 		        .map(resp -> ResponseEntity.ok(resp))
 		        .orElse(ResponseEntity.notFound().build());
 	}
-
+	@PostMapping("/new")
+	public ResponseEntity<Clientes> post(@RequestBody Clientes clientes){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(clientes));
+	}
+	@PutMapping("/edit/{id}")
+	public ResponseEntity<Clientes> put(@RequestBody Clientes clientes){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(clientes));
+	}
+	@DeleteMapping("/delete/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
--- sistema-financeiro/src/main/java/com/example/sistemafinanceiro/resourses/EnderecoResourses.java
+++ sistema-financeiro/src/main/java/com/example/sistemafinanceiro/resourses/EnderecoResourses.java
@@ -3,10 +3,15 @@ package com.example.sistemafinanceiro.resourses;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +36,16 @@ public class EnderecoResourses {
 		        .map(resp -> ResponseEntity.ok(resp))
 		        .orElse(ResponseEntity.notFound().build());
 	}
-
+	@PostMapping("/new")
+	public ResponseEntity<Endereco> post(@RequestBody Endereco endereco){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(endereco));
+	}
+	@PutMapping("/edit")
+	public ResponseEntity<Endereco> put(@RequestBody Endereco endereco){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(endereco));
+	}
+	@DeleteMapping ("/delete/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
